af84eebe66d05ee64f1715e2af1961a586dfb0c7
Connie Leighton <constancealeighton@gmail.com>
2018-08-27 08:31:59 +0100

    added notation

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const getList = function(){
+  // JSON.parse changes a string object into javascript
   if (JSON.parse(localStorage.getItem('films')) !== null){
+    // get from 'dictionary' value that matched the 'key' 'films'
+  // parse takes object back from the string and changes it to js
     return JSON.parse(localStorage.getItem('films'));
   } else {
     return [];
@@ -15,15 +18,19 @@ const getList = function(){
 
 const handleFormSubmit = function(event){
   event.preventDefault();
+  // getList is a list of all the films or an empty array
   filmList = getList();
+  // creating a new film from the what was submitted
   const newFilm = {
     title: event.target.title.value,
     artist: event.target.artist.value,
     release: event.target.release.value
   };
+  // new film pushed into filmList
   filmList.push(newFilm);
 
   localStorage.setItem('films', JSON.stringify(filmList));
+  // because you've added a film it needs to be shown
   renderList();
   event.target.reset();
 };
@@ -47,10 +54,16 @@ const buildList = function(film){
 
 const renderList = function(){
     const filmDiv = document.querySelector('#film-list');
+      // changing reading-list into variable and ensuring it is blank
     filmDiv.innerHTML = "";
+    // reading from JSON file (new updated :D). Converting getList from string to
+// array of films (filmList), which is what
+// get list does. Will include new film.
   const filmList = getList();
+  // making HTML from details of book
   filmList.forEach((film) => {
     filmUl = buildList(film);
+    // once html is created it is added to the empty Div
     filmDiv.appendChild(filmUl);
 
   });
